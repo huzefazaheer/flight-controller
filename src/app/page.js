eb81ca29c06fcadb7f8241164ac19b27673302fe
Huzefa Zaheer <huzefazaheer1@gmail.com>
2025-07-03 14:48:51 +0500

    Updated styling

--- src/app/page.js
+++ src/app/page.js
@@ -5,8 +5,12 @@ export default function Home() {
   return (
    <div className={styles.wrapper}>
    <div className={styles.screen}>
-    <img className={styles.bigscreen} src="/live-map.png" alt="" />
-    <img className={styles.smallscreen} src="/live-feed.webp" alt="" />
+    <div className={styles.bigscreen} >
+      <img className={styles.img} src="/live-map.png" alt="" />
+    </div>
+    <div className={styles.smallscreen}>
+      <img className={styles.img} src="/live-feed.webp" alt="" />
+    </div>
    </div>
    <div className={styles.rightmenu} >
     <div className={styles.options}>

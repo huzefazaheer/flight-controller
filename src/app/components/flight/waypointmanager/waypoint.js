72c36cc777bd00435a3817546977862982986f2d
Huzefa Zaheer <huzefazaheer1@gmail.com>
2025-07-03 21:50:06 +0500

    Added input for coordinate

--- src/app/components/flight/waypointmanager/waypoint.js
+++ src/app/components/flight/waypointmanager/waypoint.js
@@ -6,7 +6,6 @@ export default function WaypointManager(){
     const [toggleModal, setModalToggle] = useState(false);
 
     const [waypoints, setWaypoints] = useState([]);
-    const [wpId, setWpId] = useState(0);
 
     function getToggleModal(){
         if(toggleModal) return false;
@@ -48,6 +47,9 @@ export default function WaypointManager(){
 
 function Modal({toggleModal, setModalToggle, waypoints, setWaypoints}){
 
+    const [waypoint, setWaypoint] = useState({});
+    const [wpId, setWpId] = useState(0);
+
     function getToggleModal(){
         if(toggleModal) return false;
         else return true;
@@ -55,10 +57,17 @@ function Modal({toggleModal, setModalToggle, waypoints, setWaypoints}){
 
     return(
         <div className={`${styles.modal} ${toggleModal ? styles.hidden : ''}`}>
-            <button className={styles.btn} onClick={(e) => {
-                setWaypoints([...waypoints, {id:4, lat:5.223132, long:4.555321, alt: 55}])
+            <div className={styles.innermodal}>
+                <div className={styles.input}><label>Latitude</label><input type="text" value={waypoint.lat} onChange={(e) => {setWaypoint({...waypoint, lat:e.target.value})}}></input></div>
+                <div className={styles.input}><label>Longitude</label><input type="text" value={waypoint.long}  onChange={(e) => {setWaypoint({...waypoint, long:e.target.value})}}></input></div>
+                <div className={styles.input}><label>Altitude</label><input type="text" value={waypoint.alt}  onChange={(e) => {setWaypoint({...waypoint, alt:e.target.value})}}></input></div>
+                <button className={styles.btn} onClick={(e) => {
+                setWaypoint({...waypoint, id:wpId})
+                setWpId(wpId + 1)
+                setWaypoints([...waypoints, waypoint])
                 setModalToggle(getToggleModal())}
             }>Add New Waypoint</button>
+            </div>
         </div>
 
     );

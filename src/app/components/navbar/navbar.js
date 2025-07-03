da3bf699a78847a03469ed88dab1be160ab4793b
Huzefa Zaheer <huzefazaheer1@gmail.com>
2025-07-03 23:58:15 +0500

    Api requests to get weather and time data

--- src/app/components/navbar/navbar.js
+++ src/app/components/navbar/navbar.js
@@ -1,15 +1,35 @@
+"use client"
+
+import { useEffect, useState } from "react";
 import styles from "./navbar.module.css"
 
 export default function Navbar(){
+
+    const [weatherData, setWeatherData] = useState();
+
+    const now = new Date();
+    const time12hr = now.toLocaleTimeString('en-US', {
+    hour: 'numeric',
+    minute: '2-digit',
+    hour12: true // Force 12-hour format
+    });
+
+    const getData = useEffect(async (e) => {
+        const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/islamabad?unitGroup=us&key=ZSH978YYTT645P7GB7LHBTQHT&contentType=json");
+        const data = await response.json()
+        setWeatherData(data)
+    }, [])
 
     return(
         <nav className={styles.navbar}>
             <p className={styles.logo}>CSN</p>
             <div className={styles.status} ><img src="./status.svg" alt="" /><span className={styles.statustxt}>Status: Active</span></div>
             <div className={styles.right}>
-                <p className={styles.weather}>Clear, 30 ºC</p>
-                <p className={styles.time}>11:59 AM</p>
+                <p className={styles.weather}>{weatherData != undefined ? weatherData.currentConditions.conditions + ", " + ((weatherData.currentConditions.temp - 32) * (5/9)).toFixed(1) + " ºC" : ""}</p>
+                <p className={styles.time}>{time12hr}</p>
             </div>
         </nav>
     );
 }
+
+// tolocaletimestring

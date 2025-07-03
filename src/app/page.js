ac416e5b45c5829c3c946aedf0d0a57d3ba2fb0f
Huzefa Zaheer <huzefazaheer1@gmail.com>
2025-07-03 13:30:17 +0500

    Added sidebar menu

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,15 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
+   <div className={styles.wrapper}>
    <div className={styles.screen}>
     <img className={styles.bigscreen} src="/live-map.png" alt="" />
     <img className={styles.smallscreen} src="/live-feed.webp" alt="" />
+   </div>
+   <div className={styles.rightmenu} >
+    <div className={styles.options}></div>
+   </div>
+
    </div>
   );
 }

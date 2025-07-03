79f3ee8e5117e445d084219bb67e3d8c154c145d
Huzefa Zaheer <huzefazaheer1@gmail.com>
2025-07-03 14:59:05 +0500

    Added toggling of map and live feed

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,30 @@
+"use client"
+
 import styles from "./page.module.css";
 import MenuButton from "./components/button/button";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [mapMinimised, setMapMinimised] = useState(false);
+
+  function toggleMap(){
+    if(mapMinimised) return false;
+    else return true;
+  }
+
   return (
    <div className={styles.wrapper}>
    <div className={styles.screen}>
     <div className={styles.bigscreen} >
-      <img className={styles.img} src="/live-map.png" alt="" />
+      <img className={styles.img} src={!mapMinimised ? "/live-map.png" : "/live-feed.webp"} alt="" />
     </div>
     <div className={styles.smallscreen}>
-      <img className={styles.img} src="/live-feed.webp" alt="" />
+      <img className={styles.img} src={mapMinimised ? "/live-map.png" : "/live-feed.webp"} alt="" />
+      <div className={styles.controls}>
+        <p>{!mapMinimised ? "Live Feed" : "Map"}</p>
+        <img src="/maximise.svg" alt="" onClick={(e) => {setMapMinimised(toggleMap());}}/>
+      </div>
     </div>
    </div>
    <div className={styles.rightmenu} >

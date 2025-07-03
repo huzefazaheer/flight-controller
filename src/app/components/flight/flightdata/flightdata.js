937c1c150e9c2e7a157d930ab97002a98e6259ad
Huzefa Zaheer <huzefazaheer1@gmail.com>
2025-07-03 15:26:16 +0500

    Created flight data section

--- src/app/components/flight/flightdata/flightdata.js
+++ src/app/components/flight/flightdata/flightdata.js
@@ -1,5 +1,44 @@
+import styles from "./flightdata.module.css"
+
 export default function FlightData(){
     return(
-        <p>Flight Data</p>
+        <div className={styles.flightdata}>
+            <div className={styles.maindataholder}>
+            <div className={styles.maindata}>
+                <p className={styles.dataheading}>Title</p>
+                <h1 className={styles.datatxt}>0.00</h1>
+            </div>
+            <div className={styles.maindata}>
+                <p className={styles.dataheading}>Title</p>
+                <h1 className={styles.datatxt}>0.00</h1>
+            </div>
+            <div className={styles.maindata}>
+                <p className={styles.dataheading}>Title</p>
+                <h1 className={styles.datatxt}>0.00</h1>
+            </div>
+            <div className={styles.maindata}>
+                <p className={styles.dataheading}>Title</p>
+                <h1 className={styles.datatxt}>0.00</h1>
+            </div>
+            <div className={styles.maindata}>
+                <p className={styles.dataheading}>Title</p>
+                <h1 className={styles.datatxt}>0.00</h1>
+            </div>
+            <div className={styles.maindata}>
+                <p className={styles.dataheading}>Title</p>
+                <h1 className={styles.datatxt}>0.00</h1>
+            </div>
+        </div>
+        <div className={styles.pos}>
+            <h3>Position</h3>
+            <p className={styles.alttxt}>33° 38' 34.839" N</p>
+            <p className={styles.alttxt}>72° 59' 26.768" E</p>
+        </div>
+        <div className={styles.pos}>
+            <h3>Heading</h3>
+            <p className={styles.alttxt}>33° 38' 34.839" N</p>
+            <p className={styles.alttxt}>72° 59' 26.768" E</p>
+        </div>
+        </div>
     )
 }

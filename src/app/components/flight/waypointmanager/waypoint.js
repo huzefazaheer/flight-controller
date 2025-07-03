4fdb0ff9451a0acea5acba022f0caf74017825df
Huzefa Zaheer <huzefazaheer1@gmail.com>
2025-07-03 21:10:31 +0500

    Added onClick to new waypoint maker

--- src/app/components/flight/waypointmanager/waypoint.js
+++ src/app/components/flight/waypointmanager/waypoint.js
@@ -1,17 +1,35 @@
+import { use, useState } from "react";
 import styles from "./waypoint.module.css"
 
 export default function WaypointManager(){
+
+    const [waypoints, setWaypoints] = useState([]);
+    const [wpId, setWpId] = useState(0);
+
+    const tabledata = waypoints.map(wp => {
+        return(
+            <tr key={crypto.randomUUID()}>
+                    <td>{wp.id}</td>
+                    <td>{wp.lat}</td>
+                    <td>{wp.long}</td>
+                    <td>{wp.alt}</td>
+            </tr>
+        );
+    })
+
     return(
         <div className={styles.waypoint}>
             <h3>Current Waypoints</h3>
-            <table>
+            <div className={styles.tablewrapper}>
+                 <table>
                 <tr>
                 <th>ID</th>
                 <th>Lat</th>
                 <th>Long</th>
                 <th>Alt</th>
                 </tr>
-                <tr>
+                {tabledata}
+                {/* <tr>
                     <td>1</td>
                     <td>5.272838</td>
                     <td>6.3163127</td>
@@ -28,9 +46,10 @@ export default function WaypointManager(){
                     <td>6.653321</td>
                     <td>7.3553167</td>
                     <td>129</td>
-                </tr>
+                </tr> */}
             </table>
-            <button className={styles.btn}>Add New Waypoint</button>
+            </div>
+            <button className={styles.btn} onClick={(e) => {setWaypoints([...waypoints, {id:4, lat:5.223132, long:4.555321, alt: 55}])}}>Add New Waypoint</button>
         </div>
     )
 }
